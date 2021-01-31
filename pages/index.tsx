0524138b87e206bd3f1581cf5bd72278bcb0e6fc
Roman Vesely <crazko@gmail.com>
2021-01-31 19:10:08 +0100

    Update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,24 @@ import { useCollection } from '@nandorojo/swr-firestore';
 import { Event, parseDates } from '../app/event';
 import { Showcase } from '../components/Showcase';
 
+const title = 'Upcoming Events';
+
 const Home = () => {
   const { data } = useCollection<Event>('events', {
     parseDates,
+    limit: 5,
+    orderBy: ['dateStart', 'desc'],
   });
+
+  console.log({ data });
 
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>{title}</title>
       </Head>
+
+      <h1>{title}</h1>
 
       {data ? <Showcase events={data} /> : <div>loading</div>}
     </>
