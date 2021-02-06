3e154715932ec67e2caf4fad4cc21141c5e03edd
Roman Vesely <crazko@gmail.com>
2021-02-06 11:58:03 +0100

    Add add event functionality

--- pages/add.tsx
+++ pages/add.tsx
@@ -1,13 +1,20 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { Form } from 'react-final-form';
+import firebase from 'firebase/app';
+import { useCollection } from '@nandorojo/swr-firestore';
 
 import { useAuth } from 'app/AuthProvider';
+import { Event } from 'app/event';
+import { EventForm } from 'components/EventForm';
 
 const title = 'Add Event';
 
-const Add: React.FC = () => {
+const AddEventPage: React.FC = () => {
   const { isLoggedIn } = useAuth();
   const router = useRouter();
+
+  const { add } = useCollection('/events');
 
   if (!isLoggedIn) {
     if (typeof window !== 'undefined') {
@@ -17,6 +24,33 @@ const Add: React.FC = () => {
     return null;
   }
 
+  const onSubmit = async (values: Event) => {
+    // TODO: Runs spinner
+    try {
+      await add({
+        name: values.name,
+        summary: values.summary,
+        dateStart: values.dateStart,
+        dateEnd: values.dateEnd,
+        ...(values.type === 'online'
+          ? { type: 'online', url: values.url }
+          : {
+              type: 'local',
+              location: new firebase.firestore.GeoPoint(values.location.latitude, values.location.longitude),
+            }),
+      });
+
+      router.push(`/`);
+
+      // toast
+    } catch (e) {
+      // toast
+      throw e;
+    } finally {
+      // TODO: remove spinner
+    }
+  };
+
   return (
     <>
       <Head>
@@ -24,8 +58,10 @@ const Add: React.FC = () => {
       </Head>
 
       <h1>{title}</h1>
+
+      <Form onSubmit={onSubmit} initialValues={{ type: 'online' }} component={EventForm} />
     </>
   );
 };
 
-export default Add;
+export default AddEventPage;
