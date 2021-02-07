278b1a9c4268b50bec9bed7899c17e843614b69d
Roman Vesely <crazko@gmail.com>
2021-02-07 20:33:10 +0100

    Add link to explanation

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -34,9 +34,16 @@ export const Layout: React.FC = ({ children }) => {
         {children}
       </main>
 
-      <footer className="p-5 text-center border-t-2 border-red-200 text-xs uppercase">
+      <footer className="p-5 text-center border-t-2 border-red-200 text-xs uppercase space-x-2">
         <a href="https://github.com/crazko/kalendino" className="text-gray-600 hover:underline">
           source
+        </a>
+        <span>&bull;</span>
+        <a
+          href="https://gist.github.com/crazko/b421859b8b8fd71e43668f0de7668992"
+          className="text-gray-600 hover:underline"
+        >
+          thoughts
         </a>
       </footer>
     </>
